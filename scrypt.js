6f67f110aa0f1c26ad45f850945b28f2aa64fc7b
Vincasmen <vincasmen13@gmail.com>
2023-08-23 17:49:52 +0200

    Added swiper

--- scrypt.js
+++ scrypt.js
@@ -7,3 +7,49 @@ menuIcon.addEventListener("click", () => {
   menuList.style.display =
     menuList.style.display === "block" ? "none" : "block";
 });
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  breakpoints: {
+    // when window width is >= 320px
+    1024: {
+      spaceBetween: 10,
+    },
+    1440: {
+      spaceBetween: 40,
+    },
+    2560: {
+      spaceBetween: 100,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
